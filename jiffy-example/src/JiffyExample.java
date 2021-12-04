affc01953859fccafbaf8c7959a269d4ffe67b0a
Alec Xiang <alec.xiang@yale.edu>
2021-12-04 13:59:59 -0500

    chroniclemap successfully using jiffy to back its files. the commented out code in jiffyexample doesn't quite work (though we don't seem to need it for our uses) and 'make test' fails for me, but hopefully we don't need those functionalities

--- jiffy-example/src/JiffyExample.java
+++ jiffy-example/src/JiffyExample.java
@@ -2,41 +2,101 @@ import java.nio.ByteBuffer;
 import java.util.Collections;
 
 import jiffy.JiffyClient;
+import jiffy.storage.FileWriter;
+import jiffy.storage.FileReader;
 import jiffy.storage.HashTableClient;
 import jiffy.notification.HashTableListener;
 import jiffy.directory.directory_service.Client;
 import jiffy.notification.event.Notification;
 import jiffy.util.ByteBufferUtils;
 
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+
+import java.io.File;
+import java.io.IOException;
 
 public class JiffyExample {
 
+    private static ChronicleMap<String, Long> createChronicleMap(JiffyClient client) throws IOException {
+        String[] filePrefixes = {
+            "namespaceKeyStateNameToValue",
+        };
+        File[] files = createPersistedFiles(filePrefixes, client);
+
+        ChronicleMapBuilder<String, Long> cmapBuilder =
+                ChronicleMapBuilder.of(String.class, Long.class)
+                        .name("key-and-namespace-to-values")
+                        .entries(1_000_000);
+        cmapBuilder.averageKeySize(64);
+
+        return cmapBuilder.createPersistedTo(files[0]);
+    }
+
+    private static File[] createPersistedFiles(String[] filePrefixes, JiffyClient client) throws IOException {
+        File[] files = new File[filePrefixes.length];
+        for (int i = 0; i < filePrefixes.length; i++) {
+
+            try {
+                FileWriter writer = client.createFile(
+                "/BackendChronicleMaps/JiffyExample/"
+                + filePrefixes[i] 
+                + ".txt", 
+                "local://tmp"
+            );
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            
+
+            files[i] = new File(
+                "/tmp/BackendChronicleMaps/JiffyExample/"
+                + filePrefixes[i] 
+                + ".txt"
+            );
+            // new File(
+            //                 OS.getTarget()
+            //                         + "/BackendChronicleMaps/"
+            //                         + this.className
+            //                         + "/"
+            //                         + filePrefixes[i]
+            //                         + "_"
+            //                         + Integer.toString(this.numKeyedStatesBuilt)
+            //                         + ".dat");
+
+            files[i].getParentFile().mkdirs();
+            // files[i].delete();
+            // files[i].createNewFile();
+        }
+        return files;
+    }
+
     public static void main(String... args) throws Exception {
-        System.out.println("hello world");
 
         JiffyClient client = new JiffyClient("127.0.0.1", 9090, 9091);
-        String op1 = "put";
-        ByteBuffer key = ByteBufferUtils.fromString("key1");
-        ByteBuffer value = ByteBufferUtils.fromString("value1");
-        client.createHashTable("/a/file.txt", "local://tmp", 1, 1);
-        // Create listener for specific file
-        HashTableListener n1 = client.listenOnHashTable("/a/file.txt");
-        // Subscribe for a operation
-        n1.subscribe(Collections.singletonList(op1));
+        ChronicleMap kvStore = createChronicleMap(client);
 
-        HashTableClient kv = client.openHashTable("/a/file.txt");
-        kv.put(key, value);
-        kv.remove(key);
+        for(int i=1; i<=100; i++){
+            kvStore.put(String.valueOf(i), 1L*i);
+            System.out.println(kvStore.get(String.valueOf(i)));
+        }
+        
 
-        // Receive notification
-        Notification N1 = n1.getNotification();
-        System.out.println(N1);
+        // FileWriter writer = client.createFile("/a/file.txt", "local://tmp");
+        // FileReader reader = client.openFile("/a/file.txt");
 
-        // Unsubscribe operation
-        n1.unsubscribe(Collections.singletonList(op1));
+        // ByteBuffer buf = ByteBufferUtils.fromString("abcdefghijklmnopqrstuvwxy");
+        // writer.write(buf);
 
-        client.close("/a/file.txt");
-        client.remove("/a/file.txt");
+        // reader.seek(0);
+        // buf = reader.read(3);
+        // buf.rewind();
+        // byte[] arr = new byte[buf.remaining()];
+        // System.out.println(buf.get(arr));
+
+        client.remove("/BackendChronicleMaps/JiffyExample/namespaceKeyStateNameToValue.txt");
+
         client.close();
     }
     
