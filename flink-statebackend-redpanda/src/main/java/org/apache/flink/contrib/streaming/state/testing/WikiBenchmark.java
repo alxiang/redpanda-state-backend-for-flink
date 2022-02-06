d45a5875d16078786c6fa156a9a7e77c977c0f67
Alec Xiang <alec.xiang@yale.edu>
2022-02-05 19:37:37 -0500

    added command line inputs to WikiBenchmark so we don't have to recompile

--- flink-statebackend-redpanda/src/main/java/org/apache/flink/contrib/streaming/state/testing/WikiBenchmark.java
+++ flink-statebackend-redpanda/src/main/java/org/apache/flink/contrib/streaming/state/testing/WikiBenchmark.java
@@ -17,11 +17,39 @@ public class WikiBenchmark {
 
 		// set up the streaming execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		// env.setStateBackend(new RedpandaStateBackend());
-		// env.setStateBackend(new HashMapStateBackend());
-		env.setStateBackend(new EmbeddedRocksDBStateBackend());
 		env.getConfig().setParallelism(1);
 		env.disableOperatorChaining();
+
+		System.out.println("args: " + args[0]);
+		System.out.println("args: " + args[1]);
+		System.out.println("args: " + args[2]);
+
+		if(args.length >= 1){
+			if(args[0].equals("redpanda")){
+				System.out.println("Using RedpandaStateBackend");
+				env.setStateBackend(new RedpandaStateBackend());
+			}
+			else if(args[0].equals("hashmap")){
+				System.out.println("Using HashMapStateBackend");
+				env.setStateBackend(new HashMapStateBackend());
+			}
+			else if(args[0].equals("rocksdb")){
+				System.out.println("Using RocksDBStateBackend");
+				env.setStateBackend(new EmbeddedRocksDBStateBackend());
+			}
+		}
+
+		String TOPIC = "Wiki";
+		boolean async = false;
+
+		if(args.length >= 2){
+			if(args[1].equals("true")){
+				async = true;
+			}
+		}
+		if(args.length >= 3){
+			TOPIC = args[2];
+		}	
 
 		// configure source
         DataStreamSource<String> source = env.readTextFile("file:///Users/alecxiang/flink-1.13.2/wikipedia/wiki-100k.txt").setParallelism(5);
@@ -29,7 +57,7 @@ public class WikiBenchmark {
         DataStream<Tuple2<String, Long>> tokenized = source.flatMap(new Tokenizer()).setParallelism(5);
 
 		DataStream<Tuple2<String, Long>> mapper = tokenized.keyBy(record -> record.f0)
-				.flatMap(new WordCountMap("Wiki", true)) 
+				.flatMap(new WordCountMap(TOPIC, async)) 
 				.slotSharingGroup("map");
 
 		mapper.addSink(new DiscardingSink<>())
