14e9a0644663923889e85d748473b0e05e779204
Alec Xiang <alec.xiang@yale.edu>
2022-02-15 16:13:44 -0500

    add multiprocessing

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -4,6 +4,7 @@ import datetime;
 import json
 import subprocess
 import sys
+import time
 
 root_path = "/home/alec/flink-1.13.2/redpanda-state-backend-for-flink"# pathlib.Path(__file__).parent.parent.absolute()
 print(root_path)
@@ -59,32 +60,31 @@ def run_experiment_trials(args):
         result = []
         for i in range(k):
             print(f"Starting Trial {i}")
+
+            # clear the redpanda topic (if using redpanda backend)
+            if(backend == "redpanda"):
+
+                print(f"cleaning redpanda topic: {benchmark}")
+                
+                output = subprocess.run([
+                    'rpk',
+                    'topic',
+                    'delete',
+                    benchmark
+                ], capture_output=True)
+                
+                assert(output.returncode == 0)
+
             procs = []
             for t in range(jobs):
                 print(f"Submitting Job {t}")
                 procs.append(launch_flink_job(args, flink_path, root_path))
 
-
-            # output = subprocess.run([
-            #     f"{flink_path}/bin/flink",
-            #     "run",
-            #     "-m",
-            #     f"localhost:{port}",
-            #     "-c", 
-            #     f"org.apache.flink.contrib.streaming.state.testing.{benchmark_map[benchmark]}",
-            #     f"{root_path}/flink-statebackend-redpanda/target/flink-statebackend-redpanda-1.13.2-jar-with-dependencies.jar",
-            #     backend,
-            #     "true", # whether to use redpanda async batching
-            #     benchmark, # topic
-            #     "192.168.122.131" # master machine address
-            # ], capture_output=True)
-
             for t, proc in enumerate(procs):
                 while proc.poll() is None:
-                    sleep(1)
-                    pass
+                    time.sleep(1) # block until the job is ready for collection
                     
-                text_output = proc.stdout.read()
+                text_output = proc.stdout.read().decode("utf-8")
             
                 start_location = text_output.find("Job Runtime: ")+len("Job Runtime: ")
                 if start_location < len("Job Runtime: "):
@@ -95,7 +95,7 @@ def run_experiment_trials(args):
                 else:
                     end_location = start_location+text_output[start_location:].find("ms") #Not sure if this is safe to get the time if it isn't always in ms
                     time_taken = int(text_output[start_location:end_location])
-                    print(f"Trial {i} finished with time {time_taken} ms")
+                    print(f"Job {t} (Trial {i}) finished with time {time_taken} ms")
                     result.append({"trial": i, "time": time_taken, "job": t})
 
         json.dump(result, file, indent=4)
