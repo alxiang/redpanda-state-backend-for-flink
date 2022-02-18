9b7c1ba1fe01249ddb5ce14abdf6b65f1ffb84dc
Alec Xiang <alec.xiang@yale.edu>
2022-02-18 11:37:23 -0500

    add port forwarding to startup script

--- flink-kubernetes/kube-startup.sh
+++ flink-kubernetes/kube-startup.sh
@@ -3,3 +3,13 @@ kubectl create -f jobmanager-service.yaml
 kubectl create -f jobmanager-session-deployment.yaml
 kubectl create -f taskmanager-session-deployment.yaml
 
+PODNAME=$(kubectl get pods --selector=component=jobmanager -o name --no-headers=true)
+while ! kubectl port-forward "$PODNAME" 8888:8081
+do
+    echo waiting for jobmanager container to start
+    sleep 2
+done
+
+
+
+
