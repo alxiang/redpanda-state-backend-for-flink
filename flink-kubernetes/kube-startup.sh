7071b183b1804818cde376f2e4024f2c314586a9
Alec Xiang <alec.xiang@yale.edu>
2022-02-18 11:25:55 -0500

    helper scripts

---
+++ flink-kubernetes/kube-startup.sh
@@ -0,0 +1,5 @@
+kubectl create -f flink-configuration-configmap.yaml
+kubectl create -f jobmanager-service.yaml
+kubectl create -f jobmanager-session-deployment.yaml
+kubectl create -f taskmanager-session-deployment.yaml
+
---
+++ flink-kubernetes/kube-teardown.sh
@@ -0,0 +1,5 @@
+kubectl delete -f flink-configuration-configmap.yaml
+kubectl delete -f jobmanager-service.yaml
+kubectl delete -f jobmanager-session-deployment.yaml
+kubectl delete -f taskmanager-session-deployment.yaml
+
