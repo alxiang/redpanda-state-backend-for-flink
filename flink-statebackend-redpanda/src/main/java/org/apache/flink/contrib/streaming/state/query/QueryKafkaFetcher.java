d841cd44b103b4b2c3cdf342249726920efb7494
Alec Xiang <alec.xiang@yale.edu>
2022-04-25 20:43:13 -0400

    implement checkpointing functionality

--- flink-statebackend-redpanda/src/main/java/org/apache/flink/contrib/streaming/state/query/QueryKafkaFetcher.java
+++ flink-statebackend-redpanda/src/main/java/org/apache/flink/contrib/streaming/state/query/QueryKafkaFetcher.java
@@ -13,7 +13,32 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+// QuestDB imports
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.CairoEngine;
+import io.questdb.cairo.DefaultCairoConfiguration;
+import io.questdb.cairo.TableWriter;
+import io.questdb.cairo.sql.PageFrame;
+import io.questdb.cairo.sql.PageFrameCursor;
+import io.questdb.griffin.SqlCompiler;
+import io.questdb.griffin.SqlException;
+import io.questdb.griffin.SqlExecutionContextImpl;
+import io.questdb.std.Os;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.util.Properties;
+
 public class QueryKafkaFetcher<T> extends KafkaFetcher<T> {
+
+    private CairoEngine engine;
+    private SqlCompiler compiler;
+    private SqlExecutionContextImpl ctx;
+    private String table_name = "wikitable";
+
     public QueryKafkaFetcher(
             SourceFunction.SourceContext<T> sourceContext,
             Map<KafkaTopicPartition, Long> assignedPartitionsWithInitialOffsets,
@@ -43,14 +68,50 @@ public class QueryKafkaFetcher<T> extends KafkaFetcher<T> {
                 subtaskMetricGroup,
                 consumerMetricGroup,
                 useMetrics);
+
+        final CairoConfiguration configuration = new DefaultCairoConfiguration("/home/alec/.questdb");
+
+        // CairoEngine is a resource manager for embedded QuestDB
+        this.engine = new CairoEngine(configuration);
+
+        // Execution context is a conduit for passing SQL execution artefacts to the execution site
+        ctx = new SqlExecutionContextImpl(engine, 1);
+        compiler = new SqlCompiler(engine);
+
+        try {
+            // Replace live with staging
+            this.compiler.compile(
+                "create table "+table_name+"_prod (word string, count long, ts timestamp) timestamp(ts)", 
+                ctx
+            );
+        } catch (SqlException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public HashMap<KafkaTopicPartition, Long> snapshotCurrentState() {
+    public final HashMap<KafkaTopicPartition, Long> snapshotCurrentState() throws RuntimeException {
         HashMap<KafkaTopicPartition, Long> state = super.snapshotCurrentState();
         
-        // Implement QuestDB checkpoint logic
+        /*
+        Implement QuestDB checkpoint logic
+        1. QuestDBInsertMap is continuously committing records into 'wikitable'
+        2. At this checkpoint, we should copy 'wikitable' over to 'wikitable_prod'
+        */
 
+        // drop the table if it exists and re-create it
+        try {
+            // Replace live with staging
+            this.compiler.compile(
+                "BEGIN TRAN\n"+
+                "DROP TABLE wikitable_prod;\n"+
+                "EXEC sp_rename wikitable, wikitable_prod;\n"+
+                "COMMIT", 
+                ctx
+            );
+        } catch (SqlException e) {
+            e.printStackTrace();
+        }
         return state;
     }
 }
