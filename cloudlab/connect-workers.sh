a49dd4281f3c0ca82c73aec4c3563c1d8643cfb2
Alec Xiang <alec.xiang@yale.edu>
2022-07-23 15:46:53 -0700

    update join command

--- cloudlab/connect-workers.sh
+++ cloudlab/connect-workers.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Joins the workers to the master kubelet
 
+JOIN_COMMAND="$(kubeadm token create --print-join-command)"
+
 # /etc/hosts has one line for each node plus one line for localhost, 
 NUM_NODES=$(($(wc -l < /etc/hosts)-1))
 for ((i = 1; i < NUM_NODES; i++)); do
   ssh -o StrictHostKeyChecking=no alxiang@node$i echo "hello world"
+#   ssh -o StrictHostKeyChecking=no alxiang@node$i eval $JOIN_COMMAND
 done 
