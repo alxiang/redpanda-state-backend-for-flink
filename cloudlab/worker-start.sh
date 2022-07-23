d3fdcc4a1a52a0e7a1deb69d6951b6bb0d0f0b5d
Alec Xiang <alec.xiang@yale.edu>
2022-07-23 14:54:39 -0700

    allow worker to be ssh'ed into by master

--- cloudlab/worker-start.sh
+++ cloudlab/worker-start.sh
@@ -3,3 +3,6 @@
 sudo swapoff -a
 curl -sL https://gist.githubusercontent.com/alexellis/e8bbec45c75ea38da5547746c0ca4b0c/raw/23fc4cd13910eac646b13c4f8812bab3eeebab4c/configure.sh | sudo sh
 sudo systemctl stop redpanda
+
+# allow the master to ssh into us
+cat /local/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
