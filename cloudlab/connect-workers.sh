9ae82685a4a3a222c2e2d0d2635a6dce1cbcaa22
alxiang <alxiang@node0.alxiang-130376.yaleserverless-pg0.utah.cloudlab.us>
2022-07-27 22:20:05 -0600

    fixes

--- cloudlab/connect-workers.sh
+++ cloudlab/connect-workers.sh
@@ -6,6 +6,6 @@ JOIN_COMMAND="$(sudo kubeadm token create --print-join-command)"
 # /etc/hosts has one line for each node plus one line for localhost, 
 NUM_NODES=$(($(wc -l < /etc/hosts)-1))
 for ((i = 1; i < NUM_NODES; i++)); do
-  ssh -o StrictHostKeyChecking=no alxiang@node$i echo "hello world"
-#   ssh -o StrictHostKeyChecking=no alxiang@node$i eval $JOIN_COMMAND
+  ssh -o StrictHostKeyChecking=no alxiang@node$i ./local/flink-1.13.2/redpanda-state-backend-for-flink/cloudlab/worker-start.sh
+  ssh -o StrictHostKeyChecking=no alxiang@node$i eval $JOIN_COMMAND
 done 
