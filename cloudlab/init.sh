0dd99edddd71471ba95bc0279fa47e241bf38d91
alxiang <alxiang@node0.alxiang-130744.yaleserverless-pg0.utah.cloudlab.us>
2022-08-02 18:21:51 -0600

    switch to kafka

--- cloudlab/init.sh
+++ cloudlab/init.sh
@@ -5,3 +5,9 @@ echo "alias k=kubectl" >> ~/.bashrc
 echo "alias python=python3.8" >> ~/.bashrc
 
 sudo apt install tmuxinator -y
+
+# Use kafka instead of redpanda
+cd /local/flink-1.13.2/redpanda-state-backend-for-flink/kafka
+wget https://dlcdn.apache.org/kafka/3.2.1/kafka_2.13-3.2.1.tgz
+tar -xzf kafka_2.13-3.2.1.tgz
+sudo systemctl stop redpanda
