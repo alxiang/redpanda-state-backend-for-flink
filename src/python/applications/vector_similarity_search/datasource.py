dff67978531ed70d2db756316eb1469ce95ce0f1
alxiang <alxiang@node0.alxiang-131221.yaleserverless-pg0.utah.cloudlab.us>
2022-08-09 23:19:53 -0600

    linger for faster producer

--- src/python/applications/vector_similarity_search/datasource.py
+++ src/python/applications/vector_similarity_search/datasource.py
@@ -16,7 +16,8 @@ def produce_vectors(args):
     producer = KafkaProducer(
         bootstrap_servers=f'{args.host}:9192',
         key_serializer=lambda k: json.dumps(k).encode('ascii'),
-        value_serializer=lambda v: json.dumps(str(v)).encode('ascii')
+        value_serializer=lambda v: json.dumps(str(v)).encode('ascii'),
+        linger_ms=50
     )
 
     for i in range(args.num_vectors):
