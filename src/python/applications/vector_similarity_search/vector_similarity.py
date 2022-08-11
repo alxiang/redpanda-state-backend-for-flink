a6e2250cb4ba98fc6693b8b66f2c2feff5fdc121
alxiang <alxiang@node0.alxiang-131262.yaleserverless-pg0.utah.cloudlab.us>
2022-08-10 23:04:31 -0600

    retries if table doesnt exist on startup

--- src/python/applications/vector_similarity_search/vector_similarity.py
+++ src/python/applications/vector_similarity_search/vector_similarity.py
@@ -50,12 +50,21 @@ def main() -> None:
 
         # Keep querying from vectortable until max_index == num_vectors-1    
         max_index = 0
+        retries = 5
         while max_index < args.num_vectors-1:
             print(f"{max_index=}")
-            postgreSQL_select_Query = f'SELECT * FROM vectortable WHERE count >= {max_index}'
-            cursor.execute(postgreSQL_select_Query)
-            print('Selecting recent rows from test table using cursor.fetchall')
-            records = cursor.fetchall()
+
+            try:
+                postgreSQL_select_Query = f'SELECT * FROM vectortable WHERE count >= {max_index}'
+                cursor.execute(postgreSQL_select_Query)
+                print('Selecting recent rows from test table using cursor.fetchall')
+                records = cursor.fetchall()
+            except Exception as e:
+                retries -= 1
+                time.sleep(5)
+                if(retries == 0):
+                    raise e
+
             max_index = do_application_logic(target, records)
             
 
